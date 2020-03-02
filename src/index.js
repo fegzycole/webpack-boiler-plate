4775f10005952d41d224867c3a3d9855d5c84e15
fegzycole <fergusoniyara@gmail.com>
2020-03-02 23:34:54 +0100

    Push files to remote

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import fooString from './foo';
+import barString from './bar-cjs';
+import './fegs.jpg';
+
+console.log(`${fooString}${barString}`);
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+// const ExamplePlugin = require('./example-plugin.js');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          },
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
+  },
+  plugins: [
+    // new ExamplePlugin()
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+};
